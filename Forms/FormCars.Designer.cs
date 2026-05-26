a990ded24ed54b8555368e59e3d50fe7557147de
isaiascuvula <bersyteinfo@gmail.com>
2026-05-26 11:47:07 +0300

    added form cars

--- Forms/FormCars.Designer.cs
+++ Forms/FormCars.Designer.cs
@@ -28,12 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormCars";
+            label1 = new Label();
+            txtRegNumber = new TextBox();
+            txtCarBrand = new TextBox();
+            label2 = new Label();
+            txtDriverName = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            nudSeats = new NumericUpDown();
+            label5 = new Label();
+            chkLuggage = new CheckBox();
+            btnSave = new Button();
+            btnDelete = new Button();
+            dgvCars = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)nudSeats).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCars).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(141, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Reg. Number:";
+            // 
+            // txtRegNumber
+            // 
+            txtRegNumber.Location = new Point(240, 16);
+            txtRegNumber.Name = "txtRegNumber";
+            txtRegNumber.Size = new Size(172, 23);
+            txtRegNumber.TabIndex = 1;
+            // 
+            // txtCarBrand
+            // 
+            txtCarBrand.Location = new Point(240, 45);
+            txtCarBrand.Name = "txtCarBrand";
+            txtCarBrand.Size = new Size(172, 23);
+            txtCarBrand.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(141, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Car Brand:";
+            // 
+            // txtDriverName
+            // 
+            txtDriverName.Location = new Point(240, 128);
+            txtDriverName.Name = "txtDriverName";
+            txtDriverName.Size = new Size(172, 23);
+            txtDriverName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(141, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Driver Name:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(141, 77);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Seats (3-10):";
+            // 
+            // nudSeats
+            // 
+            nudSeats.Location = new Point(242, 74);
+            nudSeats.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            nudSeats.Minimum = new decimal(new int[] { 3, 0, 0, 0 });
+            nudSeats.Name = "nudSeats";
+            nudSeats.Size = new Size(120, 23);
+            nudSeats.TabIndex = 7;
+            nudSeats.Value = new decimal(new int[] { 4, 0, 0, 0 });
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(141, 102);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Luggage:";
+            // 
+            // chkLuggage
+            // 
+            chkLuggage.AutoSize = true;
+            chkLuggage.Location = new Point(242, 103);
+            chkLuggage.Name = "chkLuggage";
+            chkLuggage.Size = new Size(43, 19);
+            chkLuggage.TabIndex = 9;
+            chkLuggage.Text = "Yes";
+            chkLuggage.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(154, 175);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(117, 23);
+            btnSave.TabIndex = 10;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(277, 175);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(120, 23);
+            btnDelete.TabIndex = 11;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // dgvCars
+            // 
+            dgvCars.AllowUserToAddRows = false;
+            dgvCars.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCars.Location = new Point(0, 229);
+            dgvCars.Name = "dgvCars";
+            dgvCars.ReadOnly = true;
+            dgvCars.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvCars.Size = new Size(643, 278);
+            dgvCars.TabIndex = 12;
+            dgvCars.CellContentClick += dgvCars_CellContentClick;
+            // 
+            // FormCars
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(645, 507);
+            Controls.Add(dgvCars);
+            Controls.Add(btnDelete);
+            Controls.Add(btnSave);
+            Controls.Add(chkLuggage);
+            Controls.Add(nudSeats);
+            Controls.Add(label4);
+            Controls.Add(txtDriverName);
+            Controls.Add(label3);
+            Controls.Add(txtCarBrand);
+            Controls.Add(label2);
+            Controls.Add(txtRegNumber);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FormCars";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cars Management";
+            Load += FormCars_Load;
+            ((System.ComponentModel.ISupportInitialize)nudSeats).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvCars).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtRegNumber;
+        private TextBox txtCarBrand;
+        private Label label2;
+        private TextBox txtDriverName;
+        private Label label3;
+        private Label label4;
+        private NumericUpDown nudSeats;
+        private Label label5;
+        private CheckBox chkLuggage;
+        private Button btnSave;
+        private Button btnDelete;
+        private DataGridView dgvCars;
     }
 }
--- Forms/FormCars.cs
+++ Forms/FormCars.cs
@@ -1,18 +1,145 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+using Cars.Database;
 
 namespace Cars.Forms
 {
     public partial class FormCars : Form
     {
+        private int selectedCodeTaxi = -1;
+
+        private void FormCars_Load(object sender, EventArgs e)
+        {
+            InitializeComponent();
+            LoadCars();
+        }
+
         public FormCars()
         {
             InitializeComponent();
+            LoadCars();
+        }
+
+        private void LoadCars()
+        {
+            using (var conn = new SqliteConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Cars";
+                var cmd = new SqliteCommand(sql, conn);
+                var dt = new DataTable();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    dt.Load(reader);
+                }
+
+                dgvCars.DataSource = dt;
+            }
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtRegNumber.Text) ||
+                string.IsNullOrWhiteSpace(txtCarBrand.Text) ||
+                string.IsNullOrWhiteSpace(txtDriverName.Text))
+            {
+                MessageBox.Show("Preenche todos os campos!", "Atenção",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            using (var conn = new SqliteConnection(DatabaseHelper.ConnectionString))
+            {
+                conn.Open();
+                string sql;
+
+                if (selectedCodeTaxi == -1)
+                {
+                    sql = @"INSERT INTO Cars (RegNumber, CarBrand, Seats, Luggage, DriverName)
+                            VALUES (@reg, @brand, @seats, @luggage, @driver)";
+                }
+                else
+                {
+                    sql = @"UPDATE Cars SET RegNumber=@reg, CarBrand=@brand, 
+                            Seats=@seats, Luggage=@luggage, DriverName=@driver
+                            WHERE CodeTaxi=@code";
+                }
+
+                var cmd = new SqliteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@reg", txtRegNumber.Text.Trim());
+                cmd.Parameters.AddWithValue("@brand", txtCarBrand.Text.Trim());
+                cmd.Parameters.AddWithValue("@seats", (int)nudSeats.Value);
+                cmd.Parameters.AddWithValue("@luggage", chkLuggage.Checked ? 1 : 0);
+                cmd.Parameters.AddWithValue("@driver", txtDriverName.Text.Trim());
+
+                if (selectedCodeTaxi != -1)
+                    cmd.Parameters.AddWithValue("@code", selectedCodeTaxi);
+
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show(selectedCodeTaxi == -1 ? "Carro adicionado!" : "Carro atualizado!",
+                "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ClearForm();
+            LoadCars();
+        }
+
+        private void ClearForm()
+        {
+            selectedCodeTaxi = -1;
+            txtRegNumber.Clear();
+            txtCarBrand.Clear();
+            nudSeats.Value = 4;
+            chkLuggage.Checked = false;
+            txtDriverName.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedCodeTaxi == -1)
+            {
+                MessageBox.Show("Seleciona um carro na tabela!", "Atenção",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var confirm = MessageBox.Show("Tens a certeza que queres apagar?", "Confirmar",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirm == DialogResult.Yes)
+            {
+                using (var conn = new SqliteConnection(DatabaseHelper.ConnectionString))
+                {
+                    conn.Open();
+                    var cmd = new SqliteCommand(
+                        "DELETE FROM Cars WHERE CodeTaxi=@code", conn);
+                    cmd.Parameters.AddWithValue("@code", selectedCodeTaxi);
+                    cmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Carro apagado!", "Sucesso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ClearForm();
+                LoadCars();
+            }
+        }
+
+        private void dgvCars_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+
+            var row = dgvCars.Rows[e.RowIndex];
+            selectedCodeTaxi = Convert.ToInt32(row.Cells["CodeTaxi"].Value);
+            txtRegNumber.Text = row.Cells["RegNumber"].Value.ToString();
+            txtCarBrand.Text = row.Cells["CarBrand"].Value.ToString();
+            nudSeats.Value = Convert.ToInt32(row.Cells["Seats"].Value);
+            chkLuggage.Checked = row.Cells["Luggage"].Value.ToString() == "1";
+            txtDriverName.Text = row.Cells["DriverName"].Value.ToString();
         }
     }
 }
